fbae23b3bf11f71b492410d66cd67651b577a19f
nhatminhcybersoft <minhnhatdonald2022@gmail.com>
2022-11-19 11:22:54 +0700

    them source

---
+++ baitapSpread/js/main.js
@@ -0,0 +1,12 @@
+let hoverChar = () => {
+    let string = document.querySelector(".heading").innerHTML;
+    let charList = [...string];
+    let content = "";
+    for (const char of charList) {
+        content += `
+            <span>${char}</span>
+        `;
+    }
+    document.querySelector(".heading").innerHTML = content;
+};
+hoverChar();
